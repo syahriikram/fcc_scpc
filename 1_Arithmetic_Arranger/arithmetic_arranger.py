042e5b0453d5073d7a9d942c9a8ee262357d5c68
syahriikram <syahriikram@gmail.com>
2021-08-22 16:27:56 +0800

    Updated README.md and completed Arithmetic Arranger

--- 1_Arithmetic_Arranger/arithmetic_arranger.py
+++ 1_Arithmetic_Arranger/arithmetic_arranger.py
@@ -1,16 +1,18 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, solution=False):
   import re
 
   arranged_problems = ""
   first_line = ""
   second_line = ""
   third_line = ""
+  last_line = ""
 
   if (len(problems) > 5):
     return "Error: Too many problems."
 
   for problem in problems:
-
+    
+    ans = 0
     if not ("+" in problem or "-" in problem):
         return "Error: Operator must be '+' or '-'."
     
@@ -29,19 +31,27 @@ def arithmetic_arranger(problems):
     if (len(first_op) > 4 or len(second_op) > 4):
         return "Error: Numbers cannot be more than four digits."
 
+    if operand == "+":
+      ans = int(first_op) + int(second_op)
+    else:
+      ans = int(first_op) - int(second_op)
+
     longestLen = len(first_op) if len(first_op) > len(second_op) else len(second_op)
 
     if len(first_op) == longestLen:
       first_line += "  " + first_op + "    "
       second_line += operand + " " + second_op.rjust(longestLen," ") + "    "
-      
     else:
       first_line += first_op.rjust(longestLen+2," ")  + "    "
       second_line += operand + " " + second_op + "    "
-    third_line += "".rjust(longestLen + 2, "-") + "    "
 
+    third_line += "".rjust(longestLen + 2, "-") + "    "
+    last_line += str(ans).rjust(longestLen + 2,  " ") + "    "
+
+  if solution:
+    arranged_problems = first_line[:-4] + "\n" + second_line[:-4] + "\n" + third_line[:-4] + "\n" + last_line[:-4]
+  else:
     arranged_problems = first_line[:-4] + "\n" + second_line[:-4] + "\n" + third_line[:-4] + "\n"
 
   return arranged_problems
 
-print(arithmetic_arranger(["32 + 658", "3801 - 2", "45 + 43", "123 + 49"]))
---
+++ README.md
@@ -0,0 +1,12 @@
+# FreeCodeCamp - Scientific Computing with Python
+
+## Repo details:
+- A free course by freecodecamp.org which contains beginner video lectures about Python and 5 different mini projects
+- Upon completion of 5 mini projects, a certification is awarded!
+
+### 1st project: Arithmetic Formatter
+- Development was done on replit: https://replit.com/@SyahriIkram/boilerplate-arithmetic-formatter
+- Related codes can be found in the 1_Arithmetic_Arranger folder
+- Objective: to print out a nicely formatted arithmetic table
+
+
